ac62038c241d7b5cb0974d0cf7a94344d4a4a48a
Wando Borges <wanbrwan@gmail.com>
2023-02-01 16:47:23 -0300

    Projeto iniciado

---
+++ backend/src/main/java/com/wtiinfo/wtimeta/WtiinfoApplication.java
@@ -0,0 +1,13 @@
+package com.wtiinfo.wtimeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtiinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtiinfoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wtiinfo/wtimeta/WtiinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtiinfo.wtimeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtiinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
