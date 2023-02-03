fd3f4cb05ac813c33ec3d36adaf0700d22d5f6fb
Wando Borges <wanbrwan@gmail.com>
2023-02-03 11:49:42 -0300

    Return Pageable, criteria for date, date default for field blank

--- backend/src/main/java/com/wtiinfo/wtimeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/wtiinfo/wtimeta/repositories/SaleRepository.java
@@ -1,11 +1,18 @@
 package com.wtiinfo.wtimeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wtiinfo.wtimeta.entities.Sale;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-
+	//Substituindo findAll por findSales com o criterio de datas
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/wtiinfo/wtimeta/services/SaleService.java
+++ backend/src/main/java/com/wtiinfo/wtimeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.wtiinfo.wtimeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.wtiinfo.wtimeta.entities.Sale;
@@ -14,7 +18,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	//Data atual do sistema
+	private LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);//verificando data não foi informada
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);//verificando data não foi informada
+		return repository.findSales(min, max, pageable);
 	}
 }
