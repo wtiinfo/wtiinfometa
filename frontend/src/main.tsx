71689674b86cede56f9783164df3b6e67f2fd2f6
Wando Borges <wanbrwan@gmail.com>
2023-02-04 15:07:19 -0300

    Possible error in Netlify in comment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// case dark screen in Netlify uncomment window.React = React
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
